9e1e3dad1ba534f843b47087bba8124e359cc268
FelixvL <digitaleservice@gmail.com>
2022-03-09 14:47:39 +0100

    nog twee voorbeelden

--- demo/src/main/java/com/demoolc/demo/rest/TreinEndpoint.java
+++ demo/src/main/java/com/demoolc/demo/rest/TreinEndpoint.java
@@ -5,6 +5,7 @@ import com.demoolc.demo.database.Trein;
 import com.demoolc.demo.database.TreinRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -26,4 +27,23 @@ public class TreinEndpoint {
     public String dezeMethode(){
         return "Hij doet het";
     }
+    @GetMapping("/vbeen")
+    public void slaIetsOp(){
+        Trein trein = new Trein();
+        trein.setNaam("vbeen");
+        trein.setSnelheid(50);
+        tr.save(trein);
+    }
+    @GetMapping("/vbtwee/{eigennaam}")
+    public void slaIetsOp2(@PathVariable String eigennaam){
+        Trein trein = new Trein();
+        trein.setNaam(eigennaam);
+        trein.setSnelheid(50);
+        tr.save(trein);
+    }
+    @GetMapping("/vbdrie")
+    public Iterable<Trein> geefAllTreinene(){
+        return tr.findAll();
+    }
+
 }
